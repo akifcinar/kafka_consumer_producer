b0a13fa777c6a6df65979fccc76d0694ddc0ca26
akifcinar <49044721+akifcinar@users.noreply.github.com>
2023-01-21 22:58:10 +0100

    create kafka consumer produver for avro messages

---
+++ src/main/java/com/example/kafka/kafkaconsumerproducer/KafkaconsumerproducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.kafkaconsumerproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaconsumerproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaconsumerproducerApplication.class, args);
+	}
+
+}
